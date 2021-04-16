32e8fca44f55d1c79f84a06ee517198395edcdf8
marcelwillis3 <56885255+marcelwillis3@users.noreply.github.com>
2021-04-16 10:01:15 -0400

    Finish the basic website. Add POST methods.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var db = require('monk')('mongodb+srv://app-user:n0tTh3b3%24t@cluster0.ufvfo.mongodb.net/PCComponentsDB?retryWrites=true&w=majority');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -34,7 +33,6 @@ app.use(function(req, res, next){
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -53,4 +53,128 @@ router.get('/contactus', function(req, res, next) {
   res.render('contactus', {title: 'PC Building Advisor'})
 });
 
+/* POST to Add Motherboard Service */
+router.post('/addMobo', function(req, res) {
+  
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get our form values.
+  var mobo_brand = req.body.moboBrand;
+  var mobo_model = req.body.moboModel;
+  var mobo_scket = req.body.moboSocket;
+
+  // Set the collection
+  var collection = db.get('Motherboards');
+
+  // Submit to the DB
+  collection.insert({
+    "type": "motherboard",
+    "brand": mobo_brand,
+    "model": mobo_model,
+    "socket": mobo_scket
+  }, function(err, doc) {
+    if (err) {
+      // Return error if database insert failed
+      res.send("There was an issue trying to enter your motherboard into the database.");
+    } else {
+      // Forward to success page
+      res.redirect("/");
+    }
+  });
+});
+
+/* POST to Add CPU Service */
+router.post('/addCpu', function(req, res) {
+  
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get our form values.
+  var cpu_brand = req.body.cpuBrand;
+  var cpu_name = req.body.cpuName;
+  var cpu_scket = req.body.cpuSocket;
+
+  // Set the collection
+  var collection = db.get('CPUs');
+
+  // Submit to the DB
+  collection.insert({
+    "type": "cpu",
+    "brand": cpu_brand,
+    "name": cpu_name,
+    "socket": cpu_scket
+  }, function(err, doc) {
+    if (err) {
+      // Return error if database insert failed
+      res.send("There was an issue trying to enter your CPU into the database.");
+    } else {
+      // Forward to success page
+      res.redirect("/");
+    }
+  });
+});
+
+/* POST to Add GPU Service */
+router.post('/addGpu', function(req, res) {
+  
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get our form values.
+  var gpu_brand = req.body.gpuBrand;
+  var gpu_model = req.body.gpuModel;
+  var gpu_watts = req.body.gpuWatts;
+
+  // Set the collection
+  var collection = db.get('GPUs');
+
+  // Submit to the DB
+  collection.insert({
+    "type": "gpu",
+    "brand": gpu_brand,
+    "model": gpu_model,
+    "recommend_watts": gpu_watts
+  }, function(err, doc) {
+    if (err) {
+      // Return error if database insert failed
+      res.send("There was an issue trying to enter your GPU into the database.");
+    } else {
+      // Forward to success page
+      res.redirect("/");
+    }
+  });
+});
+
+/* POST to Add PSU Service */
+router.post('/addPsu', function(req, res) {
+  
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get our form values.
+  var psu_brand = req.body.psuBrand;
+  var psu_model = req.body.psuModel;
+  var psu_watts = req.body.psuWatts;
+
+  // Set the collection
+  var collection = db.get('PSUs');
+
+  // Submit to the DB
+  collection.insert({
+    "type": "psu",
+    "brand": psu_brand,
+    "model": psu_model,
+    "max_watts": psu_watts
+  }, function(err, doc) {
+    if (err) {
+      // Return error if database insert failed
+      res.send("There was an issue trying to enter your PSU into the database.");
+    } else {
+      // Forward to success page
+      res.redirect("/");
+    }
+  });
+});
+
 module.exports = router;
